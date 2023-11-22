221963070ccc22f3a9ad1721e0b2c83f3ee227ba
Boris Sturua <stboris65@gmail.com>
2023-11-22 15:06:49 +0300

    layout lesson files #2

--- src/main/java/org/example/hexlet/dto/courses/CoursePage.java
+++ src/main/java/org/example/hexlet/dto/courses/CoursePage.java
@@ -1,8 +1,8 @@
 package org.example.hexlet.dto.courses;
 // Путь src/org/example/hexlet/dto/courses/CoursePage.java
-import java.util.List;
-
 import org.example.hexlet.model.Course;
+
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
